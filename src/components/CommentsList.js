ce231fbba6f32cfd7a01eaf031f422388724db64
Polina Usatova <polina@Polinas-MacBook-Pro.local>
2023-03-17 11:07:03 +0000

    nothing working yet

--- src/components/CommentsList.js
+++ src/components/CommentsList.js
@@ -1,13 +1,32 @@
+import { useParams } from 'react-router-dom';
+
 export default function CommentsList () {
+
+    const {article_id} = useParams();
+    
+    console.log(article_id);
+
     return (
     <>
     <>Comments list will be here:</>
-    {/* //   <CommentsList />
+
+    {/* 
+     GET /api/articles/:article_id/comments
+    an array of comments for the given article_id of which each comment should have the following properties:
+comment_id
+votes
+created_at
+author
+body
+article_id
+
+    //   <CommentsList />
     //   <Vote />
     //   <AddCommentBtn />
     //   <ViewCommentsBtn />
     //   <Next />
-    //   <BackToArticleBtn /> */}
+    //   <BackToArticleBtn /> 
+    */}
     </>
 )
 }
--- src/components/SeeComments.js
+++ src/components/SeeComments.js
@@ -3,7 +3,8 @@ import { Link } from 'react-router-dom';
 
 export default function SeeComments (article_id) {
     return(<>
-    <Link  className="see-comments" to={`/articles/${article_id}/comments`}>see comments
-                    </Link>
+    <Link  className="see-comments" to={`/articles/${article_id}/comments`}>
+        see comments
+    </Link>
     </>)
 }
