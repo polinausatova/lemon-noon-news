854429b4150c26193571820641dcd0af80d1b295
Polina Usatova <polina@Polinas-MacBook-Pro.local>
2023-03-17 13:21:57 +0000

    useSearchParams added

--- src/components/Articles.js
+++ src/components/Articles.js
@@ -1,7 +1,7 @@
 import '../App.css';
 
 import { useState, useEffect } from "react";
-import { useParams } from 'react-router-dom';
+import { useSearchParams } from 'react-router-dom';
 
 import { getArticles } from "../utils/api"
 
@@ -19,35 +19,37 @@ export default function Articles ({topicsList}) {
     const [displayNumber, setNumber] = useState(5);
     const [page, setPage] = useState(1);
 
-    const params = useSearchParams();
+    const [ searchParams, setSearchParams ] = useSearchParams();
+  
+    const topicQuery = searchParams.get('topic')
+  console.log (topicQuery);
+  
+    //console.log(topic);
 
-    const { topic } = useSearchParams();
-
-    console.log(topic);
-
-    const [currentTopic, setCurrentTopic] = useState(topic);
+    //const [currentTopic, setCurrentTopic] = useState(topic);
 
     const [order_by, setOrderBy] = useState('created_at');
 
     const [order, setOrder] = useState('desc');
 
-    console.log (params);
-
-    useEffect(() => {
-        setIsLoading(true);
-        setIsError(false);
- 
-        getArticles(currentTopic, order_by, order) 
-        .then((articles) => {
-            setArticles(articles);
-            setIsLoading(false);
-        })
-        .catch((err) => {
-            setIsLoading(false);
-            setIsError(true);
-            });
-        }, [displayNumber, currentTopic, order_by, order, page, topic]
-    );
+    // useEffect(() => {
+    //         setIsLoading(true);
+    //         setIsError(false);
+    
+    //         getArticles(currentTopic, order_by, order) 
+    //         .then((articles) => {
+    //             setArticles(articles);
+    //             setIsLoading(false);
+    //         })
+    //         .catch((err) => {
+    //             setIsLoading(false);
+    //             setIsError(true);
+    //         });
+    //     }, 
+    //     [displayNumber, currentTopic, order_by, order, page, 
+    //         //topic
+    //     ]
+    // );
     
     if (isLoading) return <p>News loading...</p>;
     if (isError) return <p>Something went wrong</p>;
@@ -56,7 +58,7 @@ export default function Articles ({topicsList}) {
 
 return (
     <>
-    <ArticlesFilters setNumber={setNumber} setCurrentTopic={setCurrentTopic} currentTopic={currentTopic} setOrderBy={setOrderBy} order_by={order_by} setOrder={setOrder} order={order} topicsList = {topicsList}/>
+    {/* <ArticlesFilters setNumber={setNumber} setCurrentTopic={setCurrentTopic} currentTopic={currentTopic} setOrderBy={setOrderBy} order_by={order_by} setOrder={setOrder} order={order} topicsList = {topicsList}/> */}
 
     <ArticlesPreviews articles={articles}/>
 
